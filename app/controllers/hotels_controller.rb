05a13da687d6892b0167a94ff33446f5a10793f4
Suchon4031 <o.h215.su@gmail.com>
2023-05-30 16:53:28 +0900

    投稿フォームの作成

---
+++ app/controllers/hotels_controller.rb
@@ -0,0 +1,12 @@
+class HotelsController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+  
+end
--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,2 +1,9 @@
 class Hotel < ApplicationRecord
+  belongs_to :user
+  has_one_attached :images
+
+  validates :image, presense: true
+  validates :name, presence: true
+  validates :prefecture_id,numericality: {other_than: 1, message: "can't be blank"}
+  validates :city, presence: true
 end
