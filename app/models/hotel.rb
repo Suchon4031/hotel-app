82ab8c888201c3287549dbb229c443c6866b0a58
Suchon4031 <o.h215.su@gmail.com>
2023-05-30 18:35:27 +0900

    投稿保存処理実装

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,8 +1,10 @@
 class Hotel < ApplicationRecord
   belongs_to :user
-  has_one_attached :images
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
 
-  validates :image, presense: true
+  validates :image, presence: true
   validates :name, presence: true
   validates :prefecture_id,numericality: {other_than: 1, message: "can't be blank"}
   validates :city, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "hotels#index"
-    resources :hotels
+  resources :hotels
 end
