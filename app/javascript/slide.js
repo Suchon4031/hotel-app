849010166cc0828dd943a8405531bbbd6c44b085
Suchon4031 <o.h215.su@gmail.com>
2023-06-04 22:38:06 +0900

    slickとjquery導入

---
+++ app/javascript/slide.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', function(){
+  $('.hotel-show-image').slick({
+    arrows: true,
+    dots: true,
+    appendDots: $(".dots"),
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    variableWidth: true,
+  });
+});
