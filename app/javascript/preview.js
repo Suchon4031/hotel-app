2fa71dcd7acaacac48d53c10b1479b5125af4c3b
Suchon4031 <o.h215.su@gmail.com>
2023-06-13 17:46:26 +0900

    説明記載

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function(){
-
+  // 新規投稿・編集ページのフォームを取得
   const postForm = document.getElementById("new-form");
-
+  // プレビューを表示するためのスペースを取得
   const previewList = document.getElementById("previews");
-
+  // 新規投稿・編集ページのフォームがないならここで終了。「!」は論理否定演算子。
   if (!postForm) return null;
-
+  // 投稿できる枚数の制限を定義
   const imageLimits = 6;
 
 
@@ -36,48 +36,57 @@ document.addEventListener('DOMContentLoaded', function(){
     previewList.appendChild(previewWrapper);
   };
 
+  // file_fieldを生成・表示する関数
   const buildNewFileField = () => {
+    // 2枚目用のfile_fieldを作成
     const newFileField = document.createElement('input');
     newFileField.setAttribute('type', 'file');
     newFileField.setAttribute('name', 'hotel[images][]');
 
+    // 最後のfile_fieldを取得
     const lastFileField = document.querySelector('input[type="file"][name="hotel[images][]"]:last-child');
-
+    // nextDataIndex = 最後のfile_fieldのdata-index + 1
     const nextDataIndex = Number(lastFileField.getAttribute('data-index')) +1;
     newFileField.setAttribute('data-index', nextDataIndex);
-
+    // 追加されたfile_fieldにchangeイベントをセット
     newFileField.addEventListener("change", changedFileField);
-
+    // 生成したfile_fieldを表示
     const fileFieldsArea = document.querySelector('.click-upload');
     fileFieldsArea.appendChild(newFileField);
   };
 
+  // 指定したdata-indexを持つプレビューとfile_fieldを削除する
   const deleteImage = (dataIndex) => {
     const deletePreviewImage = document.querySelector(`.preview[data-index="${dataIndex}"]`);
     deletePreviewImage.remove();
     const deleteFileField = document.querySelector(`input[type="file"][data-index="${dataIndex}"]`);
     deleteFileField.remove();
 
+    // 画像の枚数が最大のときに削除ボタンを押した場合、file_fieldを1つ追加する
     const imageCount = document.querySelectorAll(".preview").length;
     if (imageCount == imageLimits - 1) buildNewFileField();
   };
 
+  // input要素で値の変化が起きた際に呼び出される関数の中身
   const changedFileField = (e) => {
-
+    // data-index（何番目を操作しているか）を取得
     const dataIndex = e.target.getAttribute('data-index');
 
     const file = e.target.files[0];
 
+    // fileが空 = 何も選択しなかったのでプレビュー等を削除して終了する
     if (!file) {
       deleteImage(dataIndex);
       return null;
     };
 
+    // data-indexを使用して、既にプレビューが表示されているかを確認する
     const blob = window.URL.createObjectURL(file);
 
     const alreadyPreview = document.querySelector(`.preview[data-index="${dataIndex}"]`);
 
     if (alreadyPreview) {
+      // クリックしたfile_fieldのdata-indexと、同じ番号のプレビュー画像が既に表示されている場合は、画像の差し替えのみを行う
       const alreadyPreviewImage = alreadyPreview.querySelector("img");
       alreadyPreviewImage.setAttribute("src", blob);
       return null;
@@ -85,11 +94,14 @@ document.addEventListener('DOMContentLoaded', function(){
 
     buildPreviewImage(dataIndex, blob);
     
+    // 画像の枚数制限に引っかからなければ、新しいfile_fieldを追加する
     const imageCount = document.querySelectorAll(".preview").length;
     if (imageCount < imageLimits) buildNewFileField();
   };
 
+  // input要素を取得
   const fileField = document.querySelector('input[type="file"][name="hotel[images][]"]');
 
+  // input要素で値の変化が起きた際に呼び出される関数
   fileField.addEventListener('change', changedFileField);
 });
