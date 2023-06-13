3c39edaefff43dd69c2a047bd7479001e5477a51
Suchon4031 <o.h215.su@gmail.com>
2023-06-13 12:47:23 +0900

    エラーメッセージ修正

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -5,7 +5,7 @@ class Hotel < ApplicationRecord
   belongs_to :prefecture
 
   validates :images, presence: true
-  validates :images, length: { minimum: 1, maximum: 6, message: "は1枚以上3枚以下にしてください" }
+  validates :images, length: { minimum: 1, maximum: 6, message: "は1枚以上6枚以下にしてください" }
   validates :name, presence: true
   validates :prefecture_id,numericality: {other_than: 1, message: "can't be blank"}
   validates :city, presence: true
