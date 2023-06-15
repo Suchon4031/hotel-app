10c37610e0d2a2461385059bf832a557075ca418
Suchon4031 <o.h215.su@gmail.com>
2023-06-15 18:05:42 +0900

    非同期化

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,20 @@
 class LikesController < ApplicationController
+  before_action :set_hotel
 
   def create
     like = current_user.likes.build(hotel_id: params[:hotel_id])
     like.save
-    redirect_to root_path
+    render 'create.js.erb'
   end
 
   def destroy
     like = Like.find_by(hotel_id: params[:hotel_id], user_id: current_user.id)
     like.destroy
-    redirect_to root_path
+    render 'destroy.js.erb'
   end
-  
+
+  def set_hotel
+    @hotel = Hotel.find(params[:hotel_id])
+  end
+
 end
