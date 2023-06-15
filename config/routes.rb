4db15a1f136a73c240bc2226d9b605e261c1cad2
Suchon4031 <o.h215.su@gmail.com>
2023-06-15 17:49:35 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "hotels#index"
-  resources :users, only: [:show]
   resources :hotels do
-    resources :likes, only: [:create, :destroy]
+    resource :likes, only: [:create, :destroy]
   end
+  resources :users, only: [:show]
 end
