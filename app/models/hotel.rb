c491d51fff16d970579663dea2243b7be557d8e3
Suchon4031 <o.h215.su@gmail.com>
2023-06-15 17:43:42 +0900

    ルーティング設定

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -3,10 +3,15 @@ class Hotel < ApplicationRecord
   has_many_attached :images
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
+  has_many :likes
 
   validates :images, presence: true
   validates :images, length: { minimum: 1, maximum: 6, message: "は1枚以上6枚以下にしてください" }
   validates :name, presence: true
   validates :prefecture_id,numericality: {other_than: 1, message: "can't be blank"}
   validates :city, presence: true
+
+  def liked_by?(user)
+    likes.where(user_id: user.id).exists?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "hotels#index"
   resources :users, only: [:show]
-  resources :hotels
+  resources :hotels do
+    resources :likes, only: [:create, :destroy]
+  end
 end
