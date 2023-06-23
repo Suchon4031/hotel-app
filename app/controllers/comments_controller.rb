ce0c4ab23049a4a9c3350178be79501682970b68
Suchon4031 <o.h215.su@gmail.com>
2023-06-23 23:14:16 +0900

    コメント投稿フォーム作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to hotel_path(params[:hotel_id])
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, hotel_id: params[:hotel_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "hotels#index"
   resources :hotels do
+    resources :comments, only: :create
     resource :likes, only: [:create, :destroy]
-    resources :comments, only: :crete
   end
+  
   resources :users, only: [:show] do
     member do
       get :likes
