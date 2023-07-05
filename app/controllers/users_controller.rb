cbb451ff824ae5359a62a83bb77f0aeb7c6940cf
Suchon4031 <o.h215.su@gmail.com>
2023-07-05 15:34:07 +0900

    userコントローラー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,16 @@
 class UsersController < ApplicationController
+  before_action :set_user
 
   def show
-    @user = User.find(params[:id])
     @hotels = @user.hotels.order(created_at: :desc)
   end
 
   def likes
-    @user = User.find(params[:id])
     likes = Like.where(user_id: @user.id).pluck(:hotel_id)
     @like_hotels = Hotel.find(likes)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
